df64f7cb122863db73fd03b6a59ac2e74d3f3b0b
Fiona Rose Balala <2023101766@ms.bulsu.edu.ph>
2026-04-24 02:35:44 +0800

    Update SignUp.jsx

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { GoogleLogin } from '@react-oauth/google';
-import logoWithText from "../../assets/logoWithText.png";
+import logoWithText from "../../assets/logowithtext.png";
 import googleIcon from "../../assets/icons/google.svg";
 import { register } from "../../services/auth";
 import { googleLogin } from "../../services/googleAuth";
